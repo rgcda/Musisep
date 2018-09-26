464fda16bb332dd06a945966551e6ebea3b80a03
Sören Schulze <sschulze@math.uni-bremen.de>
2018-09-26 14:21:59 +0200

    fix performance measures

--- musisep/audio/performance.py
+++ musisep/audio/performance.py
@@ -51,83 +51,6 @@ def project(signals, q_matrix):
     q_matrix = np.asarray(q_matrix)
     return q_matrix.T.dot(q_matrix.dot(signals.T)).T
 
-class MeasureAcc:
-    """
-    Container object for the computation of performance measures.
-
-    Parameters
-    ----------
-    n : int
-        Number of instruments to consider
-    """
-
-    def __init__(self, n):
-        self.n = n
-        self.target_sum = np.zeros((n, n))
-        self.proj_sum = np.zeros(n)
-        self.distortion = np.zeros((n, n))
-        self.interference = np.zeros((n, n))
-        self.artifacts = np.zeros(n)
-
-    def add(self, synth_signals, orig_signals):
-        """
-        Add portions of signals to the container.
-
-        Parameters
-        ----------
-        synth_signals : array_like
-            Matrix with the synthesized signals in its rows
-        orig_signals : array_like
-            Matrix with the original signals in its rows
-        """
-
-        synth_signals = np.asarray(synth_signals)
-        orig_signals = np.asarray(orig_signals)
-
-        orig_signals = orig_signals / np.linalg.norm(orig_signals,
-                                                     axis=1)[:, np.newaxis]
-        proj_signals = project(synth_signals, orthogonalize(orig_signals))
-        target_signals = (orig_signals.dot(synth_signals.T)[:, :, np.newaxis]
-                          * orig_signals[:, np.newaxis, :])
-        
-        self.target_sum += np.sum(np.square(target_signals), axis=2)
-        self.proj_sum += np.sum(np.square(proj_signals), axis=1)
-        self.distortion += np.sum(np.square(target_signals - synth_signals),
-                                  axis=2)
-        self.interference += np.sum(np.square(target_signals - proj_signals),
-                                    axis=2)
-        self.artifacts += np.sum(np.square(proj_signals - synth_signals),
-                                 axis=1)
-
-    def perms(self):
-        """
-        Obtain all the permutations of the signals and the related
-        performance measures
-
-        Returns
-        -------
-        perms : list of ndarray
-            Permutations of the indices of the signals
-        measures : list of ndarray
-            Arrays with SDR, SIR, and SAR for the signals in rows
-        """
-
-        perms = []
-        measures = []
-
-        sig_sar = 10 * np.log10(self.proj_sum / self.artifacts)
-        for p in it.permutations(range(self.n)):
-            idcs0 = np.asarray(p)
-            idcs1 = np.arange(self.n)
-            sig_sdr = 10 * np.log10(self.target_sum[idcs0, idcs1]
-                                    / self.distortion[idcs0, idcs1])
-            sig_sir = 10 * np.log10(self.target_sum[idcs0, idcs1]
-                                    / self.interference[idcs0, idcs1])
-            perms.append(idcs0)
-            measures.append(np.vstack((sig_sdr, sig_sir, sig_sar)))
-
-        return perms, measures
-
 def measures(synth_signals, orig_signals, size=1048576):
     """
     Compute the SDR, SIR, and SAR in all permutations of the synthesized
@@ -152,13 +75,46 @@ def measures(synth_signals, orig_signals, size=1048576):
 
     synth_signals = np.asarray(synth_signals)
     orig_signals = np.asarray(orig_signals)
+    n = orig_signals.shape[0]
 
-    m = MeasureAcc(orig_signals.shape[0])
+    target_sum = np.zeros((n, n))
+    proj_sum = np.zeros(n)
+    distortion = np.zeros((n, n))
+    interference = np.zeros((n, n))
+    artifacts = np.zeros(n)
+
+    orig_signals = orig_signals / np.linalg.norm(orig_signals,
+                                                 axis=1)[:, np.newaxis]
+    proj_signals = project(synth_signals, orthogonalize(orig_signals))
+    target_signals = (orig_signals.dot(synth_signals.T)[:, :, np.newaxis]
+                      * orig_signals[:, np.newaxis, :])
+
     pos = np.arange(0, orig_signals.shape[1], size)
     for p in pos:
-        m.add(synth_signals[:, p:p+size], orig_signals[:, p:p+size])
+        target_sum += np.sum(np.square(target_signals[:, :, p:p+size]), axis=2)
+        proj_sum += np.sum(np.square(proj_signals[:, p:p+size]), axis=1)
+        distortion += np.sum(np.square(target_signals[:, :, p:p+size]
+                                       - synth_signals[:, p:p+size]), axis=2)
+        interference += np.sum(np.square(target_signals[:, :, p:p+size]
+                                         - proj_signals[:, p:p+size]), axis=2)
+        artifacts += np.sum(np.square(proj_signals[:, p:p+size]
+                                      - synth_signals[:, p:p+size]), axis=1)
 
-    return m.perms()
+    perms = []
+    measures = []
+
+    sig_sar = 10 * np.log10(proj_sum / artifacts)
+    for p in it.permutations(range(n)):
+        idcs0 = np.asarray(p)
+        idcs1 = np.arange(n)
+        sig_sdr = 10 * np.log10(target_sum[idcs0, idcs1]
+                                / distortion[idcs0, idcs1])
+        sig_sir = 10 * np.log10(target_sum[idcs0, idcs1]
+                                / interference[idcs0, idcs1])
+        perms.append(idcs0)
+        measures.append(np.vstack((sig_sdr, sig_sir, sig_sar)))
+
+    return perms, measures
 
 def select_perm(perms, measures):
     """
